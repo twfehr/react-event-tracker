92a107d95a288eaa16b913f240fb5d7c332a5f84
Travis <39714398+twfehr@users.noreply.github.com>
2022-08-18 02:35:52 -0400

    Learned how to output dynamic data & use expressions within JSX snippet
    Use single curly brackets to create dynamic placeholders for javascript (http requests, calculations, operations, variables, etc)

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,9 +1,12 @@
 import "./ExpenseItem.css";
 
 function ExpenseItem() {
+  const expenseDate = new Date(2021, 2, 28);
+  const expenseTitle = "Car Insurance";
+  const expenseAmount = 294.67;
   return (
     <div className="expense-item">
-      <div>Date</div>
+      <div>{expenseDate.toISOString()}</div>
       <div className="expense-item__description">
         <h2>Title</h2>
         <div className="expense-item__price">Amount</div>
