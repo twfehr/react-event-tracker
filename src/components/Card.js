d47bacc4e44dee4bb1f21d3ab105b0301a2a516c
Travis <39714398+twfehr@users.noreply.github.com>
2022-08-20 13:54:09 -0400

    Created reusable wrapper component via children props
    Removed border-radius and box-shadow from ExpenseItem.css & Expenses.css to create a reusable wrapper card container component that will act as a shell around our ExpenseItem.js content or our Expenses.js content. We replaced the built-in div inside our ExpenseItem.js & Expenses.js content with Card.js & passed in original className as a prop.

---
+++ src/components/Card.js
@@ -0,0 +1,10 @@
+import "./Card.css";
+
+function Card(props) {
+  // Anything we recieve as a className from outside is added to this string
+  const classes = "card " + props.className;
+  //props.children is the content
+  return <div className={classes}>{props.children}</div>;
+}
+
+export default Card;
