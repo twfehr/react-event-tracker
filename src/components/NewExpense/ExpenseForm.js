7cf6e68a2c145882591a834c28a4f6170d5312bb
Travis <39714398+twfehr@users.noreply.github.com>
2022-08-21 15:06:26 -0400

    Handled Form Submission to output to console
    Added onSubmit to form. Prevented default page refresh. Converted date to date object and output final submission object to console.

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import "./ExpenseForm.css";
 
-const ExpenseForm = () => {
+const ExpenseForm = (props) => {
   //first element is pointer to managed data [value inside of useState, which is an object]
   //second element is function that sets new value
   const [userInput, setUserInput] = useState({
@@ -34,12 +34,34 @@ const ExpenseForm = () => {
     });
   };
 
+  //**Recreate final object with date converted */
+  //automatically receive event object
+  const submitHandler = (event) => {
+    //prevent default refreshing of page
+    event.preventDefault();
+    const expenseData = {
+      title: userInput.enteredTitle,
+      //+ converts input from a string to a number
+      amount: +userInput.enteredAmount,
+      //parse date string and convert it into a date object
+      date: new Date(userInput.enteredDate),
+    };
+    //call the prop that we passed into this component which contains the function, while passing expenseData as an argument.
+    props.onSaveExpenseData(expenseData);
+
+    setUserInput({ enteredTitle: "", enteredAmount: "", enteredDate: "" });
+  };
+
   return (
-    <form>
+    <form onSubmit={submitHandler}>
       <div className="new-expense__controls">
         <div className="new-expense__control">
           <label>Title</label>
-          <input type="text" onChange={titleChangeHandler} />
+          <input
+            type="text"
+            value={userInput.enteredTitle}
+            onChange={titleChangeHandler}
+          />
         </div>
         <div className="new-expense__control">
           <label>Amount</label>
@@ -47,6 +69,7 @@ const ExpenseForm = () => {
             type="number"
             min="0.01"
             step="0.01"
+            value={userInput.enteredAmount}
             onChange={amountChangeHandler}
           />
         </div>
@@ -56,11 +79,16 @@ const ExpenseForm = () => {
             type="date"
             min="2019-01-01"
             max="2022-12-31"
+            value={userInput.enteredDate}
             onChange={dateChangeHandler}
           />
         </div>
       </div>
       <div className="new-expense__actions">
+        {/* button type is button so it doesn't submit form, executes function in NewExpense.js */}
+        <button type="button" onClick={props.onCancel}>
+          Cancel
+        </button>
         <button type="submit">Add Expense</button>
       </div>
     </form>
