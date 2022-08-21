f2d4044c46df905afb9fadadb790f55ed39f123a
Travis <39714398+twfehr@users.noreply.github.com>
2022-08-21 16:55:29 -0400

    Achieved 2-way binding by setting input field's value property
    The input field's value property is set equal to useState's managed data pointer (which points to an object [3 pieces of data]). The form's onSubmit event triggers a submitHandler which outputs the data to console, then trigger's onState's state-updating function setUserInput while passing in a new object with the managed data set to an empty string to clear the input fields.

--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -2,10 +2,21 @@ import React from "react";
 import ExpenseForm from "./ExpenseForm";
 import "./NewExpense.css";
 
-const NewExpense = () => {
+const NewExpense = (props) => {
+  //function expects to get enteredExpenseData as a parameter
+  const saveExpenseDataHandler = (enteredExpenseData) => {
+    const expenseData = {
+      //pull out all key:value pairs and add them to this object
+      ...enteredExpenseData,
+      //add in new id key (do not use math.random in real app)
+      id: Math.random().toString(),
+    };
+    //call prop that has the function we made inside of App.js, pass expenseData as an argument
+    props.onAddExpense(expenseData);
+  };
   return (
     <div className="new-expense">
-      <ExpenseForm />
+      <ExpenseForm onSaveExpenseData={saveExpenseDataHandler} />
     </div>
   );
 };
