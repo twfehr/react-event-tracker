9f3cb0eac3d2761eb5a35e1be0a730686c345485
Travis <39714398+twfehr@users.noreply.github.com>
2022-08-21 14:13:46 -0400

    useState - Define user entered values as a state
    Created a single useState with an object for all input fields. Three different change handlers are triggered on each keystroke (onChange) which fires a state-updating function for that particular state, which fires a callback function that receives the previous value to modify the particular state that triggered the state-updating function.

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -1,21 +1,63 @@
-import React from "react";
+import React, { useState } from "react";
 import "./ExpenseForm.css";
 
 const ExpenseForm = () => {
+  //first element is pointer to managed data [value inside of useState, which is an object]
+  //second element is function that sets new value
+  const [userInput, setUserInput] = useState({
+    enteredTitle: "",
+    enteredAmount: "",
+    enteredDate: "",
+  });
+
+  const titleChangeHandler = (event) => {
+    //prevState is previous state key value pairs passed into anonymous function
+    setUserInput((prevState) => {
+      //preserve old state object and overwrite enteredTitle
+      return { ...prevState, enteredTitle: event.target.value };
+    });
+  };
+
+  const amountChangeHandler = (event) => {
+    //prevState is previous state key value pairs passed into anonymous function
+    setUserInput((prevState) => {
+      //preserve old state object and overwrite enteredAmount
+      return { ...prevState, enteredAmount: event.target.value };
+    });
+  };
+
+  const dateChangeHandler = (event) => {
+    //prevState is previous state key value pairs passed into anonymous function
+    setUserInput((prevState) => {
+      //preserve old state object and overwrite enteredDate
+      return { ...prevState, enteredDate: event.target.value };
+    });
+  };
+
   return (
     <form>
       <div className="new-expense__controls">
         <div className="new-expense__control">
           <label>Title</label>
-          <input type="text" />
+          <input type="text" onChange={titleChangeHandler} />
         </div>
         <div className="new-expense__control">
           <label>Amount</label>
-          <input type="number" min="0.01" step="0.01" />
+          <input
+            type="number"
+            min="0.01"
+            step="0.01"
+            onChange={amountChangeHandler}
+          />
         </div>
         <div className="new-expense__control">
           <label>Date</label>
-          <input type="date" min="2019-01-01" max="2022-12-31" />
+          <input
+            type="date"
+            min="2019-01-01"
+            max="2022-12-31"
+            onChange={dateChangeHandler}
+          />
         </div>
       </div>
       <div className="new-expense__actions">
